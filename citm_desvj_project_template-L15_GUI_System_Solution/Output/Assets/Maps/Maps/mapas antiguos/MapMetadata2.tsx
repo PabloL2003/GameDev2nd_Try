06b1f927b3a39400ae3ac2044d1edcd5c503b1e3
PabloL2003 <pablol.seda@gmail.com>
2023-12-22 12:34:32 +0100

    Ultima entrega

---
+++ citm_desvj_project_template-L15_GUI_System_Solution/Output/Assets/Maps/Maps/mapas antiguos/MapMetadata2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="MapMetadata2" tilewidth="32" tileheight="16" tilecount="4" columns="2">
+ <image source="../../../../../../Desktop/MapMetadata2.png" trans="ff00ff" width="67" height="34"/>
+</tileset>
---
+++ citm_desvj_project_template-L15_GUI_System_Solution/Output/Assets/Maps/Maps/mapas antiguos/file de dungeon aseprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="file de dungeon aseprite" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="../../../../../../Desktop/drive-download-20231030T075211Z-001/file de dungeon aseprite.png" width="265" height="199"/>
+</tileset>
